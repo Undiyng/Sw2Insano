7e8291f6a9944be7f6225e71aec01c82fd30c044
Eduardo Ramirez <eduard.jrl21@gmail.com>
2025-02-23 22:37:50 -0400

    anadiendo vercel

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('API Documentation')
     .setDescription('The API description')
