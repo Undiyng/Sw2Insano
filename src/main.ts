834d03b6810e39032a6d1eebe2414584895cc5fa
Luis Cortez <danielcorteznew8@gmail.com>
2025-02-23 20:08:04 -0400

    fix: dejandolo igual

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,7 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
-  // Habilitar CORS con configuración explícita
-  app.enableCors({
-    origin: '*', 
-    methods: 'GET,POST,PUT,DELETE,OPTIONS', 
-    allowedHeaders: 'Content-Type, Accept, Authorization',
-    credentials: true,
-  });
-
+  const app = await NestFactory.create(AppModule, {cors: true});
   const config = new DocumentBuilder()
     .setTitle('API Documentation')
     .setDescription('The API description')
@@ -39,7 +30,6 @@ async function bootstrap() {
     ],
   });
 
-  // Iniciar la aplicación en el puerto asignado por Vercel
-  await app.listen(process.env.PORT || 3200);
+  await app.listen(process.env.PORT ?? 3200);
 }
 bootstrap();
